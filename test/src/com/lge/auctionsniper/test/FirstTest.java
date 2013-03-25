ecf07820bec3d40f58c101e143892d2db460c95f
unknown <jooyung.han@JYHan8058.LGE.NET>
2013-03-25 10:51:04 +0900

    initial commit with failing test

---
+++ test/src/com/lge/auctionsniper/test/FirstTest.java
@@ -0,0 +1,9 @@
+package com.lge.auctionsniper.test;
+
+import junit.framework.TestCase;
+
+public class FirstTest extends TestCase {
+	public void testFirst() throws Exception {
+		fail("fail first");
+	}
+}
