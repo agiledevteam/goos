68aa3a65de0ca403cc6eea1abb9d8d0874190ce0
wonyoung <wonyoungjjang@gmail.com>
2013-03-25 16:47:06 +0900

    added connecting to the auction

--- src/com/lge/auctionsniper/MainActivity.java
+++ src/com/lge/auctionsniper/MainActivity.java
@@ -1,5 +1,12 @@
 package com.lge.auctionsniper;
 
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.MessageListener;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.packet.Message;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -18,5 +25,34 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+
+	public void join(String host, String username, String password,
+			String itemId) throws XMPPException {
+		XMPPConnection connection;
+		connection = connectTo(host, username, password);
+		Chat chat = connection.getChatManager().createChat(auctionId(itemId, connection), new MessageListener() {
+
+			@Override
+			public void processMessage(Chat aChat, Message message) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+		chat.sendMessage(new Message());
+	}
+
+	private String auctionId(String itemId, XMPPConnection connection) {
+		return String.format("auction-%s@%s",itemId, connection.getServiceName());
+	}
+
+	private XMPPConnection connectTo(String host, String username,
+			String password) throws XMPPException {
+		ConnectionConfiguration config = new ConnectionConfiguration(host, 5222);
+		XMPPConnection connection = new XMPPConnection(config);
+		
+		connection.connect();
+		connection.login(username, password);
+		return connection;
+	}
     
 }
--- test/src/com/lge/auctionsniper/test/ApplicationRunner.java
+++ test/src/com/lge/auctionsniper/test/ApplicationRunner.java
@@ -1,6 +1,11 @@
 package com.lge.auctionsniper.test;
 
+import org.jivesoftware.smack.XMPPException;
+
+import android.app.Activity;
+
 import com.jayway.android.robotium.solo.Solo;
+import com.lge.auctionsniper.MainActivity;
 
 public class ApplicationRunner {
 	private AuctionSniperDriver driver;
@@ -9,6 +14,13 @@ public class ApplicationRunner {
 	}
 
 	public void startBiddingIn(FakeAuctionServer auction, Solo solo) {
+		MainActivity mainActivity = (MainActivity) solo.getCurrentActivity();
+		try {
+			mainActivity.join("localhost", "sniper", "sniper", "item-54321");
+		} catch (XMPPException e) {
+			e.printStackTrace();
+		}
+		
 		driver = new AuctionSniperDriver(solo, 1000);
 		driver.showsSniperStatus("Joining");
 	}
