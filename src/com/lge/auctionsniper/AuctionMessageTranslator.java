6cf00b6a54ed26b3e2648d276d08bf97224b47c1
Jooyung Han <jooyung.han@gmail.com>
2013-03-25 18:21:31 +0900

    added AuctionMessageTranslator Unit tests

---
+++ src/com/lge/auctionsniper/AuctionMessageTranslator.java
@@ -0,0 +1,16 @@
+package com.lge.auctionsniper;
+
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.MessageListener;
+import org.jivesoftware.smack.packet.Message;
+
+public class AuctionMessageTranslator implements MessageListener {
+
+	public AuctionMessageTranslator(AuctionEventListener listener) {
+	}
+
+	@Override
+	public void processMessage(Chat arg0, Message arg1) {
+	}
+
+}
