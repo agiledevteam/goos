fed3a2556ba7c898002b582f4beea878851e0467
Jooyung Han <jooyung.han@gmail.com>
2013-03-26 03:00:41 +0900

    Make sniper bid when price comes

--- src/com/lge/auctionsniper/AuctionSniper.java
+++ src/com/lge/auctionsniper/AuctionSniper.java
@@ -17,6 +17,8 @@ public class AuctionSniper implements AuctionEventListener {
 
 	@Override
 	public void currentPrice(int price, int increment) {
+		auction.bid(price + increment);
+		sniperListener.sniperBidding();
 	}
 
 }
