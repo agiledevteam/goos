0433aa17b880398439afbcf7f2cb18ab82fdd29f
wonyoung <wonyoungjjang@gmail.com>
2013-04-23 17:07:33 +0900

    Added 'receives xmpp messages from sniper' unittest

--- test/src/com/lge/auctionsniper/unittest/XmppServerTest.java
+++ test/src/com/lge/auctionsniper/unittest/XmppServerTest.java
@@ -3,6 +3,7 @@ package com.lge.auctionsniper.unittest;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 
+import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -27,41 +28,39 @@ public class XmppServerTest extends TestCase {
 	public static final String AUCTION_RESOURCE = "Auction";
 
 	final SimpleXMPPUser sniper = new SimpleXMPPUser("sniper", "sniper");
-	final XMPPConnection item0 = new XMPPConnection(
-			new ConnectionConfiguration(XMPP_HOSTNAME, 5222));
-	final XMPPConnection item1 = new XMPPConnection(
-			new ConnectionConfiguration(XMPP_HOSTNAME, 5222));
-	SimpleChatManagerListener chat0 = new SimpleChatManagerListener();
-	SimpleChatManagerListener chat1 = new SimpleChatManagerListener();
-	private ExecutorService executor = Executors.newFixedThreadPool(10);
+	final SimpleXMPPUser item0 = new SimpleXMPPUser("auction-item-54321", "auction");
+	final SimpleXMPPUser item1 = new SimpleXMPPUser("auction-item-65432", "auction");
 
 	private XmppServer server = new XmppServer();
 
 	@Override
 	protected void setUp() throws Exception {
 		server.start();
-		super.setUp();
-	}
 
-	public void testReceivesAllMessages() throws Exception {
 		sniper.login();
-		login(item0, "auction-item-54321", "auction");
-		login(item1, "auction-item-65432", "auction");
-
-		item0.getChatManager().addChatListener(chat0);
-		item1.getChatManager().addChatListener(chat1);
+		item0.login();
+		item1.login();
 
 		sniper.createChat("auction-item-54321@localhost/" + AUCTION_RESOURCE);
 		sniper.createChat("auction-item-65432@localhost/" + AUCTION_RESOURCE);
+
+		waitChat(item0);
+		waitChat(item1);
+
+		super.setUp();
+	}
+
+	public void testSniperRecievesFromItems() throws Exception {
+		ExecutorService executor = Executors.newFixedThreadPool(10);
 
 		Future<?> future = executor.submit(new Runnable() {
 			@Override
 			public void run() {
 				try {
-					chat0.sendMessage("MESSAGE FROM ITEM0");
-					chat0.sendMessage("MESSAGE2 FROM ITEM0");
-					chat1.sendMessage("MESSAGE FROM ITEM1");
-					chat1.sendMessage("MESSAGE2 FROM ITEM1");
+					item0.send(0, "MESSAGE FROM ITEM0");
+					item0.send(0, "MESSAGE2 FROM ITEM0");
+					item1.send(0, "MESSAGE FROM ITEM1");
+					item1.send(0,"MESSAGE2 FROM ITEM1");
 				} catch (XMPPException e) {
 					Log.d("xmppserver",
 							String.format("Exception :%s", e.getMessage()));
@@ -73,26 +72,62 @@ public class XmppServerTest extends TestCase {
 		});
 
 		future.get();
+		waitForAWhile();
+		waitForAWhile();
+		waitForAWhile();
+		waitForAWhile();
 
+		
 		assertThat(sniper.messageLoged, containsString("MESSAGE FROM ITEM0"));
 		assertThat(sniper.messageLoged, containsString("MESSAGE2 FROM ITEM0"));
 		assertThat(sniper.messageLoged, containsString("MESSAGE FROM ITEM1"));
 		assertThat(sniper.messageLoged, containsString("MESSAGE2 FROM ITEM1"));
-
-		// sniper.close();
-		// item0.disconnect();
-		// item1.disconnect();
 	}
 
-	private void login(XMPPConnection connection, String id, String password)
-			throws XMPPException, InterruptedException {
-		connection.connect();
-		connection.login(id, password, AUCTION_RESOURCE);
+	public void testAllItemsRecievesFromSniper() throws Exception {
+		ExecutorService executor = Executors.newFixedThreadPool(10);
+		Future<?> future = executor.submit(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					sniper.send(0, "MESSAGE TO ITEM0");
+					sniper.send(0, "MESSAGE2 TO ITEM0");
+					sniper.send(1, "MESSAGE TO ITEM1");
+					sniper.send(1, "MESSAGE2 TO ITEM1");
+				} catch (XMPPException e) {
+					Log.d("xmppserver",
+							String.format("Exception :%s", e.getMessage()));
+				} catch (InterruptedException e) {
+					Log.d("xmppserver",
+							String.format("Exception :%s", e.getMessage()));
+				}
+			}
+		});
+
+		future.get();
 		waitForAWhile();
-	}
+		waitForAWhile();
+		waitForAWhile();
+		waitForAWhile();
 
+		assertThat(item0.messageLoged + item1.messageLoged, containsString("MESSAGE TO ITEM0"));
+		assertThat(item0.messageLoged + item1.messageLoged, containsString("MESSAGE2 TO ITEM0"));
+		assertThat(item0.messageLoged + item1.messageLoged, containsString("MESSAGE TO ITEM1"));
+		assertThat(item0.messageLoged + item1.messageLoged, containsString("MESSAGE2 TO ITEM1"));
+	}	
+
+	private void waitChat(SimpleXMPPUser user) throws InterruptedException {
+		int timeout = 500000;
+		while(user.chatCreated() == false) {
+			if (--timeout < 0) {
+				break;
+			}
+			waitForAWhile();
+		}
+	}
+	
 	private void waitForAWhile() throws InterruptedException {
-		Thread.sleep(30);
+		Thread.sleep(25);
 	}
 
 	@Override
@@ -107,6 +142,7 @@ public class XmppServerTest extends TestCase {
 		private String password;
 		private XMPPConnection connection;
 		public String messageLoged = "";
+		private ArrayList<Chat> chats = new ArrayList<Chat>();
 
 		public SimpleXMPPUser(String id, String password) {
 			this.id = id;
@@ -114,6 +150,15 @@ public class XmppServerTest extends TestCase {
 
 			connection = new XMPPConnection(new ConnectionConfiguration(
 					XMPP_HOSTNAME, 5222));
+		}
+
+		public boolean chatCreated() {
+			return chats.size() > 0;
+		}
+
+		public void send(int i, String message) throws XMPPException, InterruptedException {
+			chats.get(i).sendMessage(message);
+//			waitForAWhile();
 		}
 
 		public void close() {
@@ -124,6 +169,30 @@ public class XmppServerTest extends TestCase {
 			connection.connect();
 			connection.login(id, password, AUCTION_RESOURCE);
 			waitForAWhile();
+			connection.getChatManager().addChatListener(new ChatManagerListener() {
+				
+				@Override
+				public void chatCreated(Chat chat, boolean arg1) {
+					if (arg1 == false) {
+						chat.addMessageListener(new MessageListener() {
+							
+							@Override
+							public void processMessage(Chat arg0, Message arg1) {
+								messageLoged += arg1.getBody();
+								Log.d("xmppserver", String.format(
+										"received %s -> %s : %s",
+										arg0.getParticipant(), id, arg1.getBody()));
+							}
+						});
+						chats.add(chat);
+						Log.d("xmppserver",
+								String.format("chat with %s created thread(%s):%s",
+										chat.getParticipant(), chat.getThreadID(),
+										chat.toString()));
+					}
+				}
+			});
+			messageLoged = "";
 		}
 
 		public void createChat(String username) throws XMPPException, InterruptedException {
@@ -139,28 +208,11 @@ public class XmppServerTest extends TestCase {
 						}
 					});
 			chat.sendMessage("HELLO " + username);
+			chats .add(chat);
 			Log.d("xmppserver",
 					String.format("chat with %s created thread(%s):%s",
 							chat.getParticipant(), chat.getThreadID(),
 							chat.toString()));
-			waitForAWhile();
-		}
-	}
-
-	class SimpleChatManagerListener implements ChatManagerListener {
-		private Chat currentChat;
-
-		@Override
-		public void chatCreated(Chat chat, boolean arg1) {
-			currentChat = chat;
-			Log.d("xmppserver",
-					String.format("chat with %s created thread(%s):%s",
-							chat.getParticipant(), chat.getThreadID(),
-							chat.toString()));
-		}
-
-		public void sendMessage(String message) throws XMPPException, InterruptedException {
-			currentChat.sendMessage(message);
 			waitForAWhile();
 		}
 	}
